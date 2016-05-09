ba77d35a61734e95488ef9abaca69d204be1c656
klauuduu98 <klaudiawasilewska98@gmail.com>
2016-05-09 20:09:47 +0200

    small changes

--- elements/post.php
+++ elements/post.php
@@ -1,9 +1,9 @@
 <div id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     <div class="row">
-        <div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-2 col-lg-offset-2 col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <div class="col-xs-offset-1 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-1 col-sm-1 col-md-1 col-lg-1">
             <h3 class="article_date"> <?php the_date(); ?> </h3>
         </div>
-        <div class="col-xs-offset-2 col-md-offset-1 col-xs-7 col-sm-4 col-md-8 col-lg-8">
+        <div class="col-xs-offset-3 col-sm-offset-2 col-md-offset-1 col-xs-6 col-sm-6 col-md-8 col-lg-8">
             <?php
 
                 the_title( sprintf( '<h2 class="article_title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
@@ -14,11 +14,13 @@
     <div class="row">
         <div class="col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xs-11 col-sm-11 col-md-11 col-lg-11">
             <h6>
-               Autor: <?php the_author(); ?>
+                Autor: <?php the_author(); ?>
                 <span class="seperator"></span>
                 Kategoria: <?php the_category(', '); ?>
-                 <span class="seperator"></span>
-               <?php the_tags(); ?>
+                <?php if (function_exists(the_tags())) {
+                    echo '<br><span class="seperator"></span>';
+                    the_tags();
+                } ?>
             </h6>
         </div>
     </div>
--- elements/search.php
+++ elements/search.php
@@ -1,8 +1,8 @@
-<form id="searchform" class="navbar-form navbar-left" role="search" method="get" action="<?php bloginfo('url'); ?>/">
+<form id="searchform" class="navbar-form navbar-left navbar" role="search" method="get" action="<?php bloginfo('url'); ?>/">
     <div class="form-group">
-        <input id="s" type="text" name="s" class="form-control" placeholder="Search">
+        <input type="text" class="form-control control" placeholder="Search">
     </div>
     <button id="searchsubmit" type="submit" class="btn btn-default" onclick="$('searchform').submit()" >
-        <i class="fa fa-search fa-lg" aria-hidden="true"></i>
+        <i class="fa fa-search fa-lg search_button" aria-hidden="true"></i>
     </button>
 </form>
