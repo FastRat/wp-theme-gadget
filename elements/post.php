638ed17e0cf64e2b89fa10372a5d645a3269421e
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2016-05-09 22:48:40 +0200

    data w artykułach

--- elements/post.php
+++ elements/post.php
@@ -1,7 +1,24 @@
+<?php
+    ob_start();
+    if ( isset($data_post) ) {
+        the_date();
+        $data_tmp = ob_get_contents();
+        
+        if ( empty($data_tmp)==FALSE && $data_tmp !=='' && is_null($data_tmp)==FALSE){
+            $data_post = $data_tmp;
+        }
+        
+    } else {
+         the_date();
+        $data_post = ob_get_contents();
+    }
+    ob_end_clean();
+?>
+
 <div id="post-<?php the_ID(); ?>" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     <div class="row">
         <div class="col-xs-offset-1 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-1 col-sm-1 col-md-1 col-lg-1">
-            <h3 class="article_date"> <?php the_date(); ?> </h3>
+            <h3 class="article_date"> <?php  echo $data_post; ?> </h3>
         </div>
         <div class="col-xs-offset-3 col-sm-offset-2 col-md-offset-1 col-xs-6 col-sm-6 col-md-8 col-lg-8">
             <?php
