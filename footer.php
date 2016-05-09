050e077141cadff8e0eeec2885971523c621a164
klauuduu98 <klaudiawasilewska98@gmail.com>
2016-05-09 13:48:50 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,8 +1,14 @@
                 <div id="fotter" class="row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <div class="fotter_text">
-                            <p id="copyright">copyright &copy; Gadgety.th </p>
-                            <p id="author">projekt i realizacja: <a href="">Klaudia Wasilewska & Piotr Kuźnik</a></p>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <div class="fotter_text">
+                                <p id="copyright">copyright &copy; Gadgety.th</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                            <div class="fotter_text">
+                                <p id="author"> projekt i realizacja: <a href="">Klaudia Wasilewska & Piotr Kuźnik</a></p>
+                            </div>
                         </div>
                     </div>
                 </div>
