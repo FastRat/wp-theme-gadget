e41426fb499d263992a620fc66cba7c973c232f3
klauuduu98 <klaudiawasilewska98@gmail.com>
2016-05-10 10:48:58 +0200

    image

--- elements/author-bio.php
+++ elements/author-bio.php
@@ -1,17 +1,22 @@
 <div class="row">
-    <div class="col-md-4">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <hr>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <?php
             echo get_avatar(get_the_author_meta('user_mail') );
         ?>
     </div>
-    <div class="col-md-8">
+    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <h3 class="author-title"><?php echo get_the_author(); ?></h3>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <p class="author-bio">
 			<?php the_author_meta( 'description' ); ?>
 			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
