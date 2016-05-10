4e3320f46d064995b38bbbceb761a5c4c2188879
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2016-05-10 13:11:06 +0200

    autor

--- elements/author-bio.php
+++ elements/author-bio.php
@@ -6,8 +6,18 @@
 <div class="row">
     <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-2 col-sm-2 col-md-2 col-lg-2">
         <?php
-            echo get_avatar(get_the_author_meta('user_mail') );
-        ?>
+            $id = get_current_user_id();
+            global $wpdb;
+            $prefix = $wpdb->prefix;
+
+            $url = $wpdb->get_var('SELECT picture FROM ' . $prefix.'user_extends' . ' WHERE userid = ' . $id );
+             
+             if ( empty( $url ) ) {
+                 echo get_avatar(get_the_author_meta('user_mail') );
+             } else {      
+            ?>
+        <img src="<?php echo $url; ?>" />
+             <?php } ?>
     </div>
     <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
         <div class="row">
