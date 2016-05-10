26eb762650bb9b7519874aa4d855517df346119a
klauuduu98 <klaudiawasilewska98@gmail.com>
2016-05-10 13:09:34 +0200

    panel-author

--- elements/author-bio.php
+++ elements/author-bio.php
@@ -4,7 +4,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+    <div class="col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 col-xs-2 col-sm-2 col-md-2 col-lg-2">
         <?php
             echo get_avatar(get_the_author_meta('user_mail') );
         ?>
@@ -17,12 +17,10 @@
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <p class="author-bio">
-			<?php the_author_meta( 'description' ); ?>
-			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-				
-			</a>
-		</p>
+                <div class="author-bio">
+        			<?php the_author_meta( 'description' ); ?>
+        			<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"></a>
+                </div>
             </div>
         </div>
     </div>
